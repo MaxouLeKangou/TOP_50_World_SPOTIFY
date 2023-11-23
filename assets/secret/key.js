fd2ad4fe2227e546b4cdad53e74185183fa7e43e
Maxime LEGRAND <maxime.legrand111@outlook.com>
2023-11-23 19:38:51 +0100

    fix. Install env Netlify

--- assets/secret/key.js
+++ assets/secret/key.js
@@ -1,7 +1,4 @@
-import 'netlify-env';
-
 export default {
     clientId: 'facc0d43c63e44d2b7fb2c677626cc97',
     clientSecret: process.env.SPOTIFY_API_KEY,
-  };
-  
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import 'netlify-env';
+
 export default defineNuxtConfig({
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
