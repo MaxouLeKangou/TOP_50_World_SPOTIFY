7890ffd05fe8ec9fe4abd92afe683758ba91ea2a
Maxime LEGRAND <maxime.legrand111@outlook.com>
2023-11-23 19:45:36 +0100

    fix. reset code to re-test

--- assets/secret/key.js
+++ assets/secret/key.js
@@ -1,6 +1,5 @@
-import 'netlify-env';
-
 export default {
     clientId: 'facc0d43c63e44d2b7fb2c677626cc97',
-    clientSecret: process.env.SPOTIFY_API_KEY,
-};
+    clientSecret: 'd8e6ad7164f542a4b1ffb3cc2b0988f9',
+  };
+  
